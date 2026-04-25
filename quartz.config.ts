c87438bac89c482ba20bb4a96e7b24f366fd27c9
Leo <2105206+leo-dcfa@users.noreply.github.com>
2026-04-25 11:54:30 +1000

    Quartz sync: Apr 25, 2026, 11:54 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Leo's Notebook",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
