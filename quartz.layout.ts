2d6ef05e74e634a1ff84583b153a76ff6fcbc183
Leo <2105206+leo-dcfa@users.noreply.github.com>
2026-04-26 19:25:43 +1000

    Quartz sync: Apr 26, 2026, 7:25 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,6 +38,7 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
+    Component.RecentNotes(),
     Component.Explorer(),
   ],
   right: [
@@ -62,6 +63,7 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
+    Component.RecentNotes(),
     Component.Explorer(),
   ],
   right: [],
